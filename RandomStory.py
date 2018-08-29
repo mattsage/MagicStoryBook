ad212a827df9d96a44645d900d4546cf75fb5fe5
Matthew Sage <mattsage@users.noreply.github.com>
2018-08-29 14:42:02 +0100

    Update RandomStory.py

--- RandomStory.py
+++ RandomStory.py
@@ -4,8 +4,8 @@ import os, random
 def randomstory ():
 	randomfile = random.choice(os.listdir("/home/pi/music/"))
 	file = './home/pi/music/'+ randomfile
-	os.system("mpg321 + file)
-	#os.system ('mplayer' + file)
-	#os.system ('omxplayer' + file)
+	os.system("mpg321 + file")
+	#os.system ('mplayer ' + file)
+	#os.system ('omxplayer ' + file)
 
 randomstory ()
