2629429c52817b7cbe37e6f1ec5d044f53704008
Matthew Sage <mattsage@users.noreply.github.com>
2018-09-03 15:08:21 +0100

    Create MagicBook.py

---
+++ MagicBook.py
@@ -0,0 +1,23 @@
+#Magic Book
+from gpiozero import LED, MotionSensor, LightSensor
+from signal import pause
+ldr = LightSensor(26)
+light = LED(25)
+def openbook():
+	print("Light!")
+	led.on()
+	os.system("mp321 /home/pi/Twinkle.mp3")	
+	randomfile = random.choice(os.listdir("/home/pi/music/"))
+	file = '/home/pi/music/'+ randomfile
+	#os.system ('omxplayer' + file)
+	os.system('mp321' + file)
+	os.system("mp321 /home/pi/TheEnd.mp3")
+def closebook():
+	led.off()
+	print("Dark!")
+	#os.system('killall omxplayer.bin')
+	os.system('killall mpg321')
+	
+ldr.when_light = openbook
+ldr.when_dark = closebook
+pause()
